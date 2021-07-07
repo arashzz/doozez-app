e98d463d2e1a8d349a5a868fc5026f82dfc89857
Arash <arash.moeen.j@gmail.com>
2021-07-07 17:45:34 +0800

    - safe detail ui improved - tabs layout added to safe detail ui - safe invite layout added

--- app/src/main/java/com/doozez/doozez/ui/safe/SafeDetailFragment.kt
+++ app/src/main/java/com/doozez/doozez/ui/safe/SafeDetailFragment.kt
@@ -5,30 +5,31 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.doozez.doozez.R
 import com.doozez.doozez.api.ApiClient
+import com.doozez.doozez.api.enqueue
 import com.doozez.doozez.api.safe.SafeDetailResponse
 import com.doozez.doozez.databinding.FragmentSafeDetailBinding
-import com.doozez.doozez.ui.safe.adapters.SafeDetailInviteListAdapter
+import com.doozez.doozez.ui.safe.adapters.SafeDetailPagerAdapter
+import com.doozez.doozez.utils.BundleKey
+import com.doozez.doozez.utils.SafeStatus
 import com.google.android.material.snackbar.Snackbar
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-private const val ARG_SAFE_ID = "safeId"
+import com.google.android.material.tabs.TabLayoutMediator
 
 class SafeDetailFragment : Fragment() {
     private var safeId: Long = 0
+    private var userId: Long = 0
     private var _binding: FragmentSafeDetailBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            safeId = it.getLong(ARG_SAFE_ID)
+            safeId = it.getLong(BundleKey.SAFE_ID)
+            userId = it.getLong(BundleKey.USER_ID)
         }
     }
 
@@ -37,49 +38,82 @@ class SafeDetailFragment : Fragment() {
 
         _binding = FragmentSafeDetailBinding.inflate(inflater, container, false)
         val view = binding.root
+        populateTabs()
         getSafeDetails()
-        addListeners()
         return view
     }
 
-    private fun addListeners() {
-        binding.safeDetailAddInvite.setOnClickListener {
-            val navController = findNavController()
-            navController.navigate(R.id.action_nav_safe_to_nav_user_search)
+    private fun addListeners(isInitiator: Boolean) {
+        if (isInitiator) {
+            binding.safeDetailAddInvite.setOnClickListener {
+                val navController = findNavController()
+                navController.navigate(
+                    R.id.action_nav_safe_to_nav_user_search, bundleOf(
+                        BundleKey.SAFE_ID to safeId
+                    )
+                )
+            }
         }
+//        setFragmentResultListener(ResultKey.SEARCHED_USER_SELECTED) { _, bundle ->
+//            var user = bundle.getParcelable<UserDetailResponse>(BundleKey.MODEL_OBJECT)
+//            addInvite(user?.id!!, user?.firstName!!)
+//        }
     }
+
+//    private fun addInvite(recipientId: Long, recipientFirstName: String) {
+//        var body = InvitationCreateRequest()
+//        body.recipientId = recipientId
+//        body.safeId = safeId
+//        val call = ApiClient.invitationService.createInvitation(body)
+//        call.enqueue {
+//            onResponse = {
+//                Snackbar.make(binding.safeDetailContainer,
+//                    "Successfully invited $recipientFirstName", Snackbar.LENGTH_SHORT).show()
+//            }
+//            onFailure = {
+//                Log.e("SafeDetailFragment-add-invite", it?.stackTrace.toString())
+//                Snackbar.make(binding.safeDetailContainer,
+//                    "Failed to invite $recipientFirstName", Snackbar.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
     private fun getSafeDetails() {
         val call = ApiClient.safeService.getSafeByIdForUser(safeId)
-        call.enqueue(
-            object : Callback<SafeDetailResponse> {
-                override fun onResponse(
-                    call: Call<SafeDetailResponse>,
-                    response: Response<SafeDetailResponse>,
-                ) {
-                    if (response.isSuccessful && response.body() != null) {
-                        populateSafeDetails(response.body())
-                    }
+        call.enqueue {
+            onResponse = {
+                if (it.isSuccessful && it.body() != null) {
+                    populateSafeDetails(it.body())
                 }
-
-                override fun onFailure(call: Call<SafeDetailResponse>, t: Throwable) {
-                    Log.e("SafeDetailFragment", t.stackTrace.toString())
-                    Snackbar.make(binding.safeDetailContainer, "failed...", Snackbar.LENGTH_SHORT).show()
-                }
-            },
-        )
+            }
+            onFailure = {
+                Log.e("SafeDetailFragment", it?.stackTrace.toString())
+                Snackbar.make(binding.safeDetailContainer, "failed...", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun populateSafeDetails(detail: SafeDetailResponse) {
         binding.safeDetailName.text = detail.name
         binding.safeDetailMonthlyPayment.text = detail.monthlyPayment.toString()
-        binding.safeDetailStatus.text = detail.status
-        binding.safeDetailTotalInvites.text = detail.invitations.size.toString() + " participants"
-
-        with(binding.safeDetailInviteList) {
-            layoutManager = LinearLayoutManager(context)
-            adapter = SafeDetailInviteListAdapter(detail.invitations)
+        binding.safeDetailStatus.text = SafeStatus.getStatusTextForCode(detail.status!!)
+        val isInitiator = detail.initiator != userId
+        if(!isInitiator) {
+            (binding.safeDetailAddInvite.parent as? ViewGroup)?.removeView(binding.safeDetailAddInvite)
         }
+        addListeners(isInitiator)
+    }
 
+    private fun populateTabs() {
+        binding.pager.adapter = SafeDetailPagerAdapter(this, safeId)
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            var tabName = ""
+            when(position) {
+                SafeDetailPagerAdapter.TAB_DETAILS -> tabName = SafeDetailPagerAdapter.TAB_DETAILS_NAME
+                SafeDetailPagerAdapter.TAB_INVITATIONS -> tabName = SafeDetailPagerAdapter.TAB_INVITATIONS_NAME
+                SafeDetailPagerAdapter.TAB_PARTICIPANTS -> tabName = SafeDetailPagerAdapter.TAB_PARTICIPANTS_NAME
+            }
+            tab.text = tabName
+        }.attach()
     }
 }
--- app/src/main/java/com/doozez/doozez/ui/safe/adapters/SafeDetailInviteListAdapter.kt
+++ app/src/main/java/com/doozez/doozez/ui/safe/adapters/SafeDetailInviteListAdapter.kt
@@ -2,16 +2,23 @@ package com.doozez.doozez.ui.safe.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.doozez.doozez.R
 import com.doozez.doozez.api.invitation.InvitationDetailResponse
 import com.doozez.doozez.databinding.FragmentSafeDetailInviteItemBinding
+import com.doozez.doozez.utils.InvitationStatus
 
 class SafeDetailInviteListAdapter(
-    private val values: List<InvitationDetailResponse>):
+    private val values: MutableList<InvitationDetailResponse>):
         RecyclerView.Adapter<SafeDetailInviteListAdapter.ViewHolder>() {
 
+    fun addItems(items: List<InvitationDetailResponse>) {
+        values.clear()
+        values.addAll(items)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         return ViewHolder(
@@ -29,12 +36,24 @@ class SafeDetailInviteListAdapter(
         val item = values[position]
         holder.name.text = item.recipient?.firstName + " " + item.recipient?.lastName
         holder.email.text = item.recipient?.email
+        holder.status.setImageResource(getStatusIcon(item.status!!))
+    }
+
+    private fun getStatusIcon(code: String): Int {
+        var id = R.drawable.ic_baseline_access_time_24
+        if (code == InvitationStatus.ACCEPTED) {
+            id = R.drawable.ic_round_check_24
+        } else if (code == InvitationStatus.DECLINED) {
+            id = R.drawable.ic_baseline_clear_24
+        }
+        return id
     }
 
     inner class ViewHolder(binding: FragmentSafeDetailInviteItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val container: LinearLayout = binding.safeDetailInviteItemContainer
+        val container: RelativeLayout = binding.safeDetailInviteItemContainer
         val name: TextView = binding.safeDetailInviteItemName
         val email: TextView = binding.safeDetailInviteItemEmail
+        val status: ImageView = binding.safeDetailInviteItemStatus
 
         override fun toString(): String {
             return super.toString() + " '"
