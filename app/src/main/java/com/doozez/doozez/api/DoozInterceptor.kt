3db50cfc014060a4b73be9d52fef919c1e28ff88
Arash <arash.moeen.j@gmail.com>
2021-07-13 10:23:08 +0800

    - okhttp3 interceptor is now reading the api token from the encrypted shared pref

--- app/src/main/java/com/doozez/doozez/api/DoozInterceptor.kt
+++ app/src/main/java/com/doozez/doozez/api/DoozInterceptor.kt
@@ -1,20 +1,20 @@
 package com.doozez.doozez.api
 
+import com.doozez.doozez.utils.SharedPrerfKey
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 class DoozInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        //admin
-//        val header1 = "YWRtaW5AZG9vemV6LmNvbTpqZXJtaW4xMjNA"
-
-        //arash
-//        val header2 = "YXJhc2hAZG9vemV6LmNvbTphcmFzaDEyM0A="
 
         val original = chain.request()
         val requestBuilder: Request.Builder = original.newBuilder()
-//        requestBuilder.addHeader("Authorization", "Basic $header1")
+
+        val apiKey = SharedPrefManager.getString(SharedPrerfKey.API_KEY, null, true)
+        if (!apiKey.isNullOrBlank()) {
+            requestBuilder.addHeader("Authorization", "Token $apiKey")
+        }
         return chain.proceed(requestBuilder.build())
     }
 }
